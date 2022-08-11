ed4c302a60dc704624c868e35ef548d504baf217
Cocytus-Leon <smilingbaboon@126.com>
2022-08-11 15:59:08 +0800

    DEBUG

--- Alignment/Demo.m
+++ Alignment/Demo.m
@@ -3,12 +3,12 @@ close all; clc; clear;
 addpath(genpath('./.'));
 folder  = './.';                                                       % Put the image pairs in this folder
 filepaths = dir(fullfile(folder, '*.png'));
-
+%%
 for i = 1:2:size(filepaths)
     I1  = im2double(imread(fullfile(folder,filepaths(i).name)));       % reference image
     I2  = im2double(imread(fullfile(folder,filepaths(i+1).name)));     % target image
 
-    s = 2;                                             % s = len1/len2, len1&lend2 are the focal length of captured image (len1>len2)
+    s = 3;                                             % s = len1/len2, len1&lend2 are the focal length of captured image (len1>len2)
     r = 1 - 1/s;                                       % Scale
     I2_zoom = warpImg(I2,[-r,0,0,0]);
     
@@ -20,6 +20,6 @@ for i = 1:2:size(filepaths)
     [I2_t_c] = color_transfer(I1,I2_t);                % transfer color
 
     % where you save the image
-    imwrite(I1, ['./.', filepaths(i).name])            
-    imwrite(I2_t_c, ['./.', filepaths(i+1).name])
+    imwrite(I1, ['./REG-', filepaths(i).name])            
+    imwrite(I2_t_c, ['./REG-', filepaths(i+1).name])
 end
